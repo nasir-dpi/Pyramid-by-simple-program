28876b860fddb7a3cbb7f6e5700f8269fe525244
nasir-dpi <uddinrjnasir@gmail.com>
2019-02-17 16:18:10 +0600

    First Commit

---
+++ src/Startpoint.java
@@ -0,0 +1,28 @@
+
+public class Startpoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i , j, k;
+		for (i = 0; i<5; i++) 
+		{
+			for(j=0;j<=5-i;j++)
+				System.out.print(" ");
+			{
+				for(k=0;k<=i;k++) {
+					System.out.print("a ");
+				}
+				System.out.println();
+				
+			}
+		
+	
+		} 
+			
+			
+	}
+		
+	}
+
+
